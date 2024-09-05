4e4fc3a3f3003d0bfbb9c2b571752bd7f4d0ed60
Shivsgithubb <vs6828208@gmail.com>
2024-09-05 19:34:22 +0530

    hhh

--- Routers/userRouter.js
+++ Routers/userRouter.js
@@ -1,25 +1,40 @@
 const express = require('express');
-
 const Model = require('../models/userModel');
+
+const jwt = require('jsonwebtoken');
+const verifyToken = require('./verifyToken');
+require('dotenv').config();
 
 const router = express.Router();
 
-router.post('/add',(req,res) => {
+router.post('/add', (req, res) => {
     console.log(req.body);
 
     new Model(req.body).save()
-    .then((result) => {
-        res.status(200).json(result);
-    })
-    .catch((err) => {
-        console.log(err);
-        if (err.code === 11000) {
-            res.status(500).json({ message: 'email already exists'})
-        } else {
-            res.status(500).json({ message: 'something went wrong'})
-        }
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        })
+        .catch((err) => {
+            console.log(err);
+            if (err.code === 11000) {
+                res.status(500).json({ message: 'Email already exists' });
+            } else {
+                res.status(500).json({ message: 'Something went wrong' });
+            }
+        });
+
+});
+
+router.get('/getall', verifyToken, (req, res) => {
+
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
 });
 
 router.get('/getbyemail/:email', (req, res) => {
@@ -42,56 +57,69 @@ router.get('/getbycity/:city', (req, res) => {
         });
 });
 
-
-
-//getall
-router.get('/getall', (req, res) => {
-
-    Model.find()
+// : denotes url parameter
+router.get('/getbyid/:id', (req, res) => {
+    Model.findById(req.params.id)
         .then((result) => {
             res.status(200).json(result);
         }).catch((err) => {
             console.log(err);
             res.status(500).json(err);
         });
-
-    });
-
-// : denotes url parameter
-router.get('/getbyid/:id',(req,res) => {
-    Model.findById(req.params.id)
-    .then((result)=> {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
 });
 
-//update
-router.put('/update/:id',(req,res) => {
-    Model.findByIdAndUpdate(req.params.id, req.body { new: true})
-    .then((result) => {
-        res.status(200).json(result);      
-    }).catch((err) => {
-        res.status(500).json(err);   
-    });
+router.put('/update/:id', (req, res) => {
+    Model.findByIdAndUpdate(req.params.id, req.body, { new: true })
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-//delete
-router.delete('/delete/:id',(req,res) => {
+router.delete('/delete/:id', (req, res) => {
     Model.findByIdAndDelete(req.params.id)
-    .then((result)=> {
-        res.status(200).json(result);
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err);
-    });
+        .then((result) => {
+            res.status(200).json(result);
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
+
+router.post('/authenticate', (req, res) => {
+    Model.findOne(req.body)
+        .then((result) => {
+            if (result) {
+
+                const { _id, name, email, password } = result;
+
+                const payload = { _id, name, email, password };
+
+                // generate token
+                jwt.sign(
+                    payload,
+                    process.env.JWT_SECRET,
+                    { expiresIn: 10 },
+                    (err, token) => {
+                        if (err) {
+                            console.log(err);
+                            res.status(500).json(err);
+                        } else {
+                            res.status(200).json({ token });
+                        }
+                    }
+                )
+
+            } else {
+                res.status(401).json({ message: 'Invalid credentials' });
+            }
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+})
 
 module.exports = router;
-
-//getall
-//getbyid
-//update
-//delete
